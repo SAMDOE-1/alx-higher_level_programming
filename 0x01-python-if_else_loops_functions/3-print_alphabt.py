4f56406dacdc343d665c61fa93fae2dff6f9cafe
SAMDOE-1 <samdoe763@gmail.com>
2022-09-20 10:24:25 -0700

    alpha

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord("a"), ord("z")+1):
-    if i == ord("q") or i == ord("e"):
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if (i != "q" and i != "e"):
         continue
 
-     print("{:c}".format(i), end="")
+    print("{:c}".format(i), end="")
