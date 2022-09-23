7960f4fd88f8518039d0c4a728193c2b8a112485
SAMDOE-1 <samdoe763@gmail.com>
2022-09-23 15:13:13 -0700

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
+import calculator_1 as calc
+import sys
 
-    num_args = len(sys.argv)
-    if num_args != 4:
+if __name__ == '__main__':
+    args = sys.argv
+    pname = args.pop(0)
+    if len(args) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
-
-    if op is '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op is '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op is '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op is '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
-    else:
+    if args[1] not in '+-*/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    a = int(args[0])
+    b = int(args[2])
+    funcs = (calc.add, calc.sub, calc.mul, calc.div)
+    for op, func in zip('+-*/', funcs):
+        if op == args[1]
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, func(a, b)))
+            break
