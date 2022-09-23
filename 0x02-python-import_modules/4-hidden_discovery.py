dec7ea941884e5991cfa75960778a6870c32ee79
SAMDOE-1 <samdoe763@gmail.com>
2022-09-23 14:42:10 -0700

    hid

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4 as 
-
-if __name__ == '__main__':
-    names = filter(lambda n: not n.startswith('__'), dir(h))
-    for name in names:
-        print(name)
+if __name__ == "__main__":
+    import hidden_4
+    all_dir = dir(hidden_4)
+    avoid = "__"
+    for i in range(0, len(all_dir)):
+        if avoid not in all_dir[i]:
+            print(all_dir[i])
